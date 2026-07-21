36be9cd9e6788695a930eeb3ebcd75280eb783ec
Rami <138556019+rami242424@users.noreply.github.com>
2026-07-21 19:43:47 +0900

    IconBtn

---
+++ src/app/components/Button.tsx
@@ -0,0 +1,23 @@
+type ButtonProps = {
+    variant: "primary" | "danger" | "success" | "default";
+    children: React.ReactNode;
+    onClick?: () => void;
+};
+
+const variantStyles = {
+    primary: "bg-violet-600 text-white",
+    danger: "bg-rose-500 text-white",
+    success: "bg-lime-300 text-slate-900",
+    default: "bg-white text-slate-900 border border-slate-300",
+};
+
+export default function Button({ variant, children, onClick } : ButtonProps){
+    return(
+        <button 
+            onClick={onClick}
+            className= {`${variantStyles[variant]} rounded-full px-4 py-2`}
+        >
+            {children}
+        </button>
+    );
+}
---
+++ src/app/components/IconBtn.tsx
@@ -0,0 +1,24 @@
+type IconBtnProps = {
+    variant?: "default" | "primary";
+    checked?: boolean;
+    onClick?: () => void;
+}
+
+export default function IconBtn({
+    variant = "default",
+    checked = false,
+    onClick,
+} : IconBtnProps){
+    const style = checked
+        ? "bg-violet-600 text-white"
+        : "bg-white border border-slate-300 text-slate-500";
+    return (
+        <button
+            onClick={onClick}
+            className={`flex h-6 w-6 items-center justify-center rounded-full ${style}`}
+            arial-label = {checked ? "완료 취소" : "완료 처리"}
+        >
+            {checked ? "✓": "" }
+        </button>
+    )
+}
