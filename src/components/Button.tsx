3eafaa2b69e62f493a602abc91c850dff760ce9b
Rami <138556019+rami242424@users.noreply.github.com>
2026-07-22 18:01:35 +0900

    feat:Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,22 +1,14 @@
 type ButtonProps = {
-    variant: "primary" | "danger" | "success" | "default";
+    type: "add" | "edit" | "delete";
+    size: "large" | "small";
+    state?: "default" | "active";
     children: React.ReactNode;
-    onClick?: () => void;
-};
+    onClick: () => void;
+}
 
-const variantStyles = {
-    primary: "bg-violet-600 text-white",
-    danger: "bg-rose-500 text-white",
-    success: "bg-lime-300 text-slate-900",
-    default: "bg-white text-slate-900 border border-slate-300",
-};
-
-export default function Button({ variant, children, onClick } : ButtonProps){
+export default function Button({children}:ButtonProps){
     return(
-        <button 
-            onClick={onClick}
-            className={`${variantStyles[variant]} whitespace-nowrap rounded-full px-4 py-2`}
-        >
+        <button>
             {children}
         </button>
     );
