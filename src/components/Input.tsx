b82e3096b60b076f6463b764e49e153616a594a4
Rami <138556019+rami242424@users.noreply.github.com>
2026-07-31 12:12:51 +0900

    feat: 인풋수정

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,18 +1,23 @@
 type InputProps = {
-    value: string;
-    onChange: (value: string) => void;
-    onKeyDown?: (e: React.KeyboardEvent<HTMLInputElement>) => void;
-    placeholder?: string;
+  value: string;
+  onChange: (value: string) => void;
+  onKeyDown?: (e: React.KeyboardEvent<HTMLInputElement>) => void;
+  placeholder?: string;
 };
 
-export default function Input ({value, onChange, placeholder, onKeyDown} : InputProps){
-    return(
-        <input
-            value={value}
-            onChange={(e) => onChange(e.target.value)}
-            placeholder={placeholder}
-            onKeyDown={onKeyDown}
-            className="h-[56px] flex-1 rounded-full border-2 border-slate-900 bg-white px-6 placeholder:text-slate-400 focus:outline-none"
-        />
-    );
+export default function Input({
+  value,
+  onChange,
+  placeholder,
+  onKeyDown,
+}: InputProps) {
+  return (
+    <input
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
+      placeholder={placeholder}
+      onKeyDown={onKeyDown}
+      className="h-[52px] flex-1 rounded-3xl border-2 border-slate-900 bg-slate-100 px-6 placeholder:text-slate-400 focus:outline-none shadow-[4px_4px_0_0_#0F172A]"
+    />
+  );
 }
